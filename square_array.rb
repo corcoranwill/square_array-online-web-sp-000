109a0a458e837837eba1484f0a727e4b6d152843
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 16:16:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  array_new = []
+  array.each do  |num|
+    array_new[counter] << num*num
+    counter += 1
+  end
+  array_new
 end
+
+# ```ruby
+# numbers = [1,2,3]
+#
+# square_array(numbers)
+# # => [1,4,9])
+#
+# new_numbers = [9,10,16,25]
+#
+# square_array(new_numbers)
+# # => [81,100,256,625]
+# ```
